6d5a75ecb20a1d71487855997e3b54199f3d9464
GksOC <guilherme.cantarino@a.unileste.edu.br>
2024-12-19 11:34:02 -0300

    CRUD Scaffolding completo e automático

---
+++ ScndMVC/Data/MainContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScndMVC.Models
+{
+    public class MainContext : DbContext
+    {
+        public MainContext (DbContextOptions<MainContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ScndMVC.Models.Department> Department { get; set; }
+    }
+}
