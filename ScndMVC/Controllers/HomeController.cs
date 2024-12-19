0f8f0e4d339f6a566a69222dd0ab5546369803a7
GksOC <guilherme.cantarino@a.unileste.edu.br>
2024-12-19 10:40:24 -0300

    Adição (básica) de uma nova página + testes com CSS e @ViewData.

--- ScndMVC/Controllers/HomeController.cs
+++ ScndMVC/Controllers/HomeController.cs
@@ -28,6 +28,12 @@ namespace ScndMVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Ridiculus viverra curae consequat ligula viverra conubia suspendisse aliquam. Mauris lacus erat aenean; efficitur ligula vel. Amet sodales eleifend maximus sagittis eu scelerisque. Praesent placerat cubilia consectetur mus volutpat ut elementum. Porta blandit dignissim malesuada dapibus odio rhoncus. Pharetra sollicitudin suspendisse eget magna eget tincidunt ullamcorper. Bibendum aliquet hendrerit viverra; turpis pharetra varius libero.\r\n\r\nUltrices ligula aliquam sodales torquent phasellus donec mi leo. Dui luctus egestas per venenatis aliquam ex. Purus dignissim mollis viverra imperdiet ac interdum. In tristique torquent; nascetur orci placerat quisque nec purus. Elit sapien amet praesent bibendum tempor primis. Hendrerit vulputate inceptos venenatis orci scelerisque scelerisque fusce elementum class. Blandit mauris aptent, est dui felis dis.";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
