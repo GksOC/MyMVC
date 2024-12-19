8cb6ee82c5e79c443bd17ff6ab822be2467ec1ec
GksOC <guilherme.cantarino@a.unileste.edu.br>
2024-12-19 11:19:01 -0300

    Alteração de ErrorViewModels para subpasta "ViewModels". Ajustando redirecionamento de namespace e using. Criação do Model "Department".

--- ScndMVC/Controllers/HomeController.cs
+++ ScndMVC/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ScndMVC.Models;
+using ScndMVC.Models.ViewModels;
 
 namespace ScndMVC.Controllers
 {
