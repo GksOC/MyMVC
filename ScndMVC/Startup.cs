5519c12780ed5122b4c3698b4d0cedb5866b49aa
GksOC <guilherme.cantarino@a.unileste.edu.br>
2024-12-23 11:24:07 -0300

    Serviço de Display Name para exibir nome das variáveis de forma que quiser no front-ent.

--- ScndMVC/Startup.cs
+++ ScndMVC/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using ScndMVC.Models;
 using ScndMVC.Data;
 using ScndMVC.Models.Services;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace ScndMVC
 {
@@ -36,6 +39,16 @@ namespace ScndMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo>() { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
