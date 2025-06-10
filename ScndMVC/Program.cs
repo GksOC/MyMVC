d1907871707215a3095b105289afc1ec1b3c9a72
GksOC <guilherme.cantarino@a.unileste.edu.br>
2025-06-10 17:08:08 -0300

    Verdadeiro HTTP

--- ScndMVC/Program.cs
+++ ScndMVC/Program.cs
@@ -11,18 +11,30 @@ namespace ScndMVC
             CreateHostBuilder(args).Build().Run();
         }
 
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            //webBuilder.ConfigureKestrel(serverOptions =>
+        //            //{
+        //            //    serverOptions.Listen(IPAddress.Any, 80, listenOptions =>
+        //            //    {
+        //            //        listenOptions.UseHttps("certs/meuCert.pfx", "3826427540028922");
+        //            //    });
+        //            //});
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
+        //versão nova
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    //webBuilder.ConfigureKestrel(serverOptions =>
-                    //{
-                    //    serverOptions.Listen(IPAddress.Any, 80, listenOptions =>
-                    //    {
-                    //        listenOptions.UseHttps("certs/meuCert.pfx", "3826427540028922");
-                    //    });
-                    //});
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+
+                // Permite que Kestrel escute em todas as interfaces de rede, porta 5000
+                webBuilder.UseUrls("http://0.0.0.0:5000");
+            });
+
     }
 }
